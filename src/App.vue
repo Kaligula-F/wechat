84f9294e643845d9544ee2f2fc79169471409566
klgl <1197945969@qq.com>
2024-06-17 22:44:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { reactive } from 'vue';
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+const config = {
+  max: 3
+}
+
+</script>
+
+<template>
+  <el-config-provider :locale="zhCn" :message="config">
+    <RouterView></RouterView>
+  </el-config-provider>
+
+</template>
+
+<style scoped></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="loginBox">
+        login
+        <el-button type="danger">Primary</el-button>
+        <el-button :plain="true" @click="open">Show message</el-button>
+        <i class="iconfont icon-phone"></i>
+        <i class="iconfont icon-yanzhengyanzhengma"></i>
+        <!-- <Home></Home> -->
+
+    </div>
+</template>
+<script setup>
+// import Home from '@/views/Home.vue';
+const open = () => {
+    ElMessage('This is a message.')
+}
+</script>
+<style lang='scss' scoped></style>s
